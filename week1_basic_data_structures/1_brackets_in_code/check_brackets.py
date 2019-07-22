178102dee4be11ca8207b24e4480e1350c27bce6
chirateep <tabechi155@gmail.com>
2019-07-22 11:36:21 +0700

    finish check brackets

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -10,21 +10,39 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
+    opening_brackets_stack = list()
     for i, next in enumerate(text):
+        # print(i, next)
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            bracket = Bracket(next, i + 1)
+            opening_brackets_stack.append(bracket)
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) > 0:
+                open_brac = opening_brackets_stack.pop()
+                match = are_matching(open_brac.char, next)
+                if match:
+                    pass
+                else:
+                    return i + 1
+            else:
+                return i + 1
+
+    # print(len(text))
+    # print(opening_brackets_stack)
+    if len(opening_brackets_stack) > 0:
+        return opening_brackets_stack.pop().position
+    else:
+        return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
