6707174f6fc6665d132555c382cdfb743050b318
chirateep <tabechi155@gmail.com>
2019-08-09 20:59:56 +0700

    finish job queue pri

--- week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,6 +1,7 @@
 # python3
 
 from collections import namedtuple
+import heapq
 
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
 
@@ -13,7 +14,25 @@ def assign_jobs(n_workers, jobs):
         next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
         result.append(AssignedJob(next_worker, next_free_time[next_worker]))
         next_free_time[next_worker] += job
+        print(next_free_time)
 
+    return result
+
+
+def fast_assign_jobs(n_workers, jobs):
+    result = []
+    next_free_time = []
+    for i in range(n_workers):
+        val = (0, i)
+        next_free_time.append(val)
+
+    heapq.heapify(next_free_time)
+
+    for job in jobs:
+        next_worker = heapq.heappop(next_free_time)
+        result.append(AssignedJob(next_worker[1], next_worker[0]))
+        heapq.heappush(next_free_time, (next_worker[0] + job, next_worker[1]))
+        # print(next_free_time)
     return result
 
 
@@ -22,7 +41,7 @@ def main():
     jobs = list(map(int, input().split()))
     assert len(jobs) == n_jobs
 
-    assigned_jobs = assign_jobs(n_workers, jobs)
+    assigned_jobs = fast_assign_jobs(n_workers, jobs)
 
     for job in assigned_jobs:
         print(job.worker, job.started_at)
