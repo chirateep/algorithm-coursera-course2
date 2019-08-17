63441de67ff09c6665b446992130b61888b19dd4
chirateep <tabechi155@gmail.com>
2019-08-18 00:29:02 +0700

    finish hash chain

--- week3_hash_tables/2_hash_chains/hash_chains.py
+++ week3_hash_tables/2_hash_chains/hash_chains.py
@@ -1,5 +1,6 @@
 # python3
 
+
 class Query:
 
     def __init__(self, query):
@@ -17,7 +18,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = dict()
 
     def _hash_func(self, s):
         ans = 0
@@ -37,26 +38,46 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
-        else:
             try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+                self.elems[query.ind].reverse()
+                self.write_chain(self.elems[query.ind])
+                self.elems[query.ind].reverse()
+            except KeyError:
+                print('')
+        else:
+            hash_val = self._hash_func(query.s)
+            ind = self.elems.get(hash_val, -1)
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                found = False
+                if ind != -1:
+                    for word in self.elems[hash_val]:
+                        if word == query.s:
+                            found = True
+                            break
+                self.write_search_result(found)
             elif query.type == 'add':
                 if ind == -1:
-                    self.elems.append(query.s)
+                    self.elems[hash_val] = [query.s]
+                else:
+                    try:
+                        ind_val = self.elems[hash_val].index(query.s)
+                    except ValueError:
+                        ind_val = -1
+                    if ind_val == -1:
+                        self.elems[hash_val].append(query.s)
             else:
                 if ind != -1:
-                    self.elems.pop(ind)
+                    for i in range(len(self.elems[hash_val])):
+                        word = self.elems[hash_val][i]
+                        if word == query.s:
+                            self.elems[hash_val].pop(i)
+                            break
 
     def process_queries(self):
         n = int(input())
         for i in range(n):
             self.process_query(self.read_query())
+
 
 if __name__ == '__main__':
     bucket_count = int(input())
