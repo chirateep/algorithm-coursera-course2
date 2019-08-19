5845744d5d0ae20e16a47c5b99192c6fbbc45f56
chirateep <tabechi155@gmail.com>
2019-08-19 23:03:35 +0700

    finish substring quality

--- week3_hash_tables/4_substring_equality/substring_equality.py
+++ week3_hash_tables/4_substring_equality/substring_equality.py
@@ -3,7 +3,7 @@ import sys
 
 m1 = 1000000007
 m2 = 1000000009
-x = 1
+x = 1124410
 
 
 class Solver:
@@ -28,13 +28,15 @@ class Solver:
         # print(a + l)
         # print(b + l)
         ans1 = (self.h1[a + l] % m1) - (pow(x, l, m1) * (self.h1[a] % m1))
-        ans2 = (self.h2[b + l] % m2) - (pow(x, l, m2) * (self.h2[b] % m2))
-        return ans1, ans2
+        ans2 = (self.h1[b + l] % m1) - (pow(x, l, m1) * (self.h1[b] % m1))
+        ans3 = (self.h2[a + l] % m2) - (pow(x, l, m2) * (self.h2[a] % m2))
+        ans4 = (self.h2[b + l] % m2) - (pow(x, l, m2) * (self.h2[b] % m2))
+        return ans1, ans2, ans3, ans4
 
     def ask(self, a, b, l):
-        ans1, ans2 = self.hash_value(a, b, l)
-        # print(ans1, ans2)
-        return ((ans1 % m1) == (ans2 % m1)) and ((ans1 % m2) == (ans2 % m2))
+        ans1, ans2, ans3, ans4 = self.hash_value(a, b, l)
+        # print(ans1, ans2, ans3, ans4)
+        return ((ans1 % m1) == (ans2 % m1)) and ((ans3 % m2) == (ans4 % m2))
 
 
 s = sys.stdin.readline()
