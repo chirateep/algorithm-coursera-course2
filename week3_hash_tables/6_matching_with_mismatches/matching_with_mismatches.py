27d041142a2a41d5cea0f952a1f31fff0bdf6fe0
chirateep.s <chirateep.s@kbtg.tech>
2019-08-23 19:13:12 +0700

    finish matching but time limit 2 sec

--- week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
+++ week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
@@ -12,30 +12,18 @@ Answer = namedtuple('answer_type', 'i j len')
 
 def solve(k, text, pattern):
     result = []
-    p_h1_pre, p_h2_pre = precompute_hash(pattern)
-    t_h1_pre, t_h2_pre = precompute_hash(text)
     for i in range(len(text) - len(pattern) + 1):
         s = t[i: i + len(pattern)]
-        s_h1_pre = t_h1_pre[i: i + len(pattern) + 1]
-        s_h2_pre = t_h2_pre[i: i + len(pattern) + 1]
         for _ in range(k):
-            max_substring = Answer(0, 0, 0)
-            # print(s_h1_pre)
+            max_substring = 0
             found = False
             left = 0
             right = len(pattern)
-            # print(s_h1_pre, s_h2_pre)
-            # print(p_h1_pre, p_h2_pre)
             while(left <= right):
                 mid = (left + right) // 2
-                # print('mid', mid)
-                # print('s', s[0:mid])
-                # print('p', p[0:mid])
-                s_h1, s_h2 = hash_value(s_h1_pre, s_h2_pre, 0, mid)
-                p_h1, p_h2 = hash_value(p_h1_pre, p_h2_pre, 0, mid)
-                if s_h1 == p_h1 and s_h2 == p_h2:
+                if s[:mid] == pattern[:mid]:
                     found = True
-                    max_substring = Answer(0, 0, mid)
+                    max_substring = mid
 
                 if found:
                     left = mid + 1
@@ -43,22 +31,12 @@ def solve(k, text, pattern):
                 else:
                     right = mid - 1
 
-            # print(max_substring)
-            if max_substring.len == len(pattern):
+            if max_substring == len(pattern):
                 break
-            pos = max_substring.len
-            # print('pos', pos)
-            # print(s[pos])
-            # print(pattern[pos])
+            pos = max_substring
             s = s[:pos] + pattern[pos] + s[pos+1:]
-            # print('s', s)
-            s_h1_pre, s_h2_pre = precompute_hash(s)
-        p_h1, p_h2 = hash_value(p_h1_pre, p_h2_pre, 0, len(pattern))
-        s_h1, s_h2 = hash_value(s_h1_pre, s_h2_pre, 0, len(s))
-        if p_h1 == s_h1 and p_h2 == s_h2:
-            # print('add')
+        if pattern == s:
             result.append(i)
-        # print('==========')
     return result
 
 
