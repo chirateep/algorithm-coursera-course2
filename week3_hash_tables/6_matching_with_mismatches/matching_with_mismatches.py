4e20b421d84f37c8b0c4ed9b20ec8092f3e11965
chirateep <tabechi155@gmail.com>
2019-08-25 03:59:17 +0700

    time limit exceed version hashing

--- week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
+++ week3_hash_tables/6_matching_with_mismatches/matching_with_mismatches.py
@@ -12,16 +12,30 @@ Answer = namedtuple('answer_type', 'i j len')
 
 def solve(k, text, pattern):
     result = []
+    p_h1_pre, p_h2_pre = precompute_hash(pattern)
+    t_h1_pre, t_h2_pre = precompute_hash(text)
     for i in range(len(text) - len(pattern) + 1):
-        s = t[i: i + len(pattern)]
+        # s = t[i: i + len(pattern)]
+        pos_mismatch = 0
+        # print('s first', text[i: i + len(pattern)])
+        # print('p first', pattern)
+        # print('i', i)
         for _ in range(k):
             max_substring = 0
             found = False
             left = 0
-            right = len(pattern)
+            right = len(pattern) - pos_mismatch
             while(left <= right):
                 mid = (left + right) // 2
-                if s[:mid] == pattern[:mid]:
+                # print('mid', mid)
+                s_h1, s_h2 = hash_value(t_h1_pre, t_h2_pre,
+                                        pos_mismatch + i, mid)
+                p_h1, p_h2 = hash_value(p_h1_pre, p_h2_pre,
+                                        pos_mismatch, mid)
+                # if s[:mid] == pattern[:mid]:
+                #     found = True
+                #     max_substring = mid
+                if s_h1 == p_h1 and s_h2 == p_h2:
                     found = True
                     max_substring = mid
 
@@ -31,12 +45,25 @@ def solve(k, text, pattern):
                 else:
                     right = mid - 1
 
-            if max_substring == len(pattern):
+            pos_mismatch += max_substring + 1
+            if max_substring >= len(pattern):
+                pos_mismatch = len(pattern)
                 break
-            pos = max_substring
-            s = s[:pos] + pattern[pos] + s[pos+1:]
-        if pattern == s:
+            if pos_mismatch >= len(pattern) or pos_mismatch >= len(text):
+                pos_mismatch = len(pattern)
+                break
+            # print('max', max_substring)
+            # print('pos', pos_mismatch)
+            # s = s[:pos] + pattern[pos] + s[pos+1:]
+        # print('last pos', pos_mismatch)
+        # print(len(pattern) - pos_mismatch)
+        s_h1, s_h2 = hash_value(t_h1_pre, t_h2_pre, i + pos_mismatch,
+                                len(pattern) - pos_mismatch)
+        p_h1, p_h2 = hash_value(p_h1_pre, p_h2_pre, pos_mismatch,
+                                len(pattern) - pos_mismatch)
+        if s_h1 == p_h1 and s_h2 == p_h2:
             result.append(i)
+        # print('-------------')
     return result
 
 
@@ -51,7 +78,9 @@ def precompute_hash(s):
 
 
 def hash_value(h1_pre, h2_pre, a, l):
-    # print(a, l)
+    # print(h1_pre)
+    # print(h2_pre)
+    # print(a, l, a + l)
     h1 = ((h1_pre[a + l] % m1) - (pow(x, l, m1) * (h1_pre[a] % m1))) % m1
     h2 = ((h2_pre[a + l] % m2) - (pow(x, l, m2) * (h2_pre[a] % m2))) % m2
     return h1, h2
