9613cfda6133138d081f34865ae3703334fac0ab
chirateep <tabechi155@gmail.com>
2019-08-28 00:36:19 +0700

    solve problem but time limit exceed

--- week4_binary_search_trees/2_is_bst/is_bst.py
+++ week4_binary_search_trees/2_is_bst/is_bst.py
@@ -1,23 +1,57 @@
 #!/usr/bin/python3
 
-import sys, threading
+import sys
+import threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
+sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
+
+key = []
+left = []
+right = []
+
+
 def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+    # Implement correct algorithm here
+    is_binary = True
+    for i in range(len(tree)):
+        key.append(tree[i][0])
+        left.append(tree[i][1])
+        right.append(tree[i][2])
+
+    for i in range(len(tree)):
+        find_pos = find(key[i], 0)
+        if find_pos == i:
+            continue
+        else:
+            is_binary = False
+            return
+    return is_binary
+
+
+def find(k, root):
+    if key[root] == k:
+        return root
+    elif key[root] > k:
+        if left[root] != -1:
+            return find(k, left[root])
+        return root
+    elif key[root] < k:
+        if right[root] != -1:
+            return find(k, right[root])
+        return root
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    if IsBinarySearchTree(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
